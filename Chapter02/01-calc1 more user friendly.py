b079f10114bf8c9e94967a29ee941faa5b9e9bc2
Vahiniaina <99581081+Vahiniaina@users.noreply.github.com>
2024-02-22 20:54:46 +0300

    Add files via upload

---
+++ Chapter02/01-calc1 more user friendly.py
@@ -0,0 +1,26 @@
+print('Hello. Input operations in the form 23 * 4. Type E to end.')         # Say hello!
+go = 1                                                                      # go is 1 to run the program
+while go == 1:                                                              # Repeat indented instructions until go not 1
+    x = input('Type first number: ')                                        # Ask for a number and call it x
+    x1 = int(x)                                                             # Convert keyboard input into an integer
+    op = input('Type operator + or - or / or *: ')                            # Ask for an operator and call it y
+    if op == 'E':                                                           # If the operator is E then 
+        go = 0                                                              #       Set go to 0 to stop
+        print('Program ended')                                              #       Say goodbye
+        break                                                               #       Jump out of the program
+    y = input('Type second number: ')                                       # Ask for a second number and call it y
+    y1 = int(y)                                                             # Convert keyboard input into an integer
+    if op == '+': 
+        result = x1 + y1 
+        print('Result = ', result, '\n')                                          # If the operator is + do addition
+    if op == '-': 
+        result = x1 - y1
+        print('Result = ', result, '\n') 
+    if op == '/': 
+        result = x1 / y1
+        print('Result = ', result, '\n')
+    if op == '*': 
+        result = x1 * y1
+        print('Result = ', result, '\n')
+    else:
+        print('Please, Type operator + or - or / or * or E\n')                                       # Print the result then repeat from "while"
